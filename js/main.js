a2332ddb0d9d9d2cc741392b172fb3ee6115dcf7
Bacon_Space <thestreamstats@gmail.com>
2016-03-03 22:47:38 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -19,7 +19,7 @@ function getData(){
          var Experience = data['user']['experience'];
          var lvls = data['user']['level'];
          var Sparks = data['user']['sparks'];
-         var Role = data['user']['groups']['name']['0'];
+         var Role = data['user']['groups'][0];
          var ChannelID = data['user']['id'];
          var Tetrisid = data['tetrisGameId'];
          var Coverid = data['coverId'];
