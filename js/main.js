6b2ab6c6fd2493a5ba3f233cae4a65ca53b4803f
Bacon_Space <thestreamstats@gmail.com>
2016-03-04 09:16:03 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -39,30 +39,30 @@ function getData(){
          var joined = data['createdAt'];
          var html = '<center><img src="' + Avatar + '"width="100px" height="100px" style="border:3px solid #fff">';
          html += '<h1><span class="label label-success">' + Username + '</h1>';
-         html += '<br><b><span class="label label-primary">----------</b>'
+         html += '<br><b><span class="label label-warning">----------</b>'
           html += '<br><b><span class="label label-primary"><font size="3" color="white">Url Link: <a href="https://beam.pro/' + Username + '">Beam.pro/'+ Username +'</a></b>';
          html += '<br><b><span class="label label-primary">Followers: ' + Followers+ '</b>';
          html += '<br><b><span class="label label-primary">User Role: ' + Role+ '</b>';
          html += '<br><b><span class="label label-primary">Partnered: ' + Partnered +'</b>';
          html += '<br><b><span class="label label-primary">Total Views: ' + TotalViews +'</b>';
          html += '<br><b><span class="label label-danger">---Points---</b>'
-         html += '<br><b><span class="label label-warning"style="color: black;">Level: ' + lvls +'</b>';
-         html += '<br><b><span class="label label-warning"style="color: black;">Experience: ' + Experience +'</b>';
-         html += '<br><b><span class="label label-warning"style="color: black;">Sparks: ' + Sparks +'</b></i>';
-         html += '<br><b><span class="label label-primary">---ID---</b>'
+         html += '<br><b><span class="label label-primary"style="color: black;">Level: ' + lvls +'</b>';
+         html += '<br><b><span class="label label-primary"style="color: black;">Experience: ' + Experience +'</b>';
+         html += '<br><b><span class="label label-primary"style="color: black;">Sparks: ' + Sparks +'</b></i>';
+         html += '<br><b><span class="label label-warning">---ID---</b>'
          html += '<br><b><span class="label label-primary">Tetris Game ID: ' + Tetrisid+ '</b>';
          html += '<br><b><span class="label label-primary">Channel ID: ' + ChannelID+ '</b>';
          html += '<br><b><span class="label label-primary">Cover ID: ' + Coverid+ '</b>';
          html += '<br><b><span class="label label-primary">Thumbnail ID: ' + Thumbnail+ '</b>';
-         html += '<br><b><span class="label label-primary">---Preference---</b>'
-         html += '<br><b><span class="label label-info">interactive:' + Tetris +'</b>';
-         html += '<br><b><span class="label label-info">Audience:' + Audience +'</b>';
-         html += '<br><b><span class="label label-info">SelfStreamMute:' + StreamMute +'</b>';
-         html += '<br><b><span class="label label-info">BeamBot FollowNotify:' + FollowNotify +'</b>';
-         html += '<br><b><span class="label label-info">BeamBot SubNotify:' + SubscribeNotify +'</b>';
-         html += '<br><b><span class="label label-info">TweetNotify:' + TweetNotify +'</b>';
-         html += '<br><b><span class="label label-info">Transcoding:' + te +'</b>';
-         html += '<br><b><span class="label label-primary">------------</b>'
+         html += '<br><b><span class="label label-warning">---Preference---</b>'
+         html += '<br><b><span class="label label-primary">interactive:' + Tetris +'</b>';
+         html += '<br><b><span class="label label-primary">Audience:' + Audience +'</b>';
+         html += '<br><b><span class="label label-primary">SelfStreamMute:' + StreamMute +'</b>';
+         html += '<br><b><span class="label label-primary">BeamBot FollowNotify:' + FollowNotify +'</b>';
+         html += '<br><b><span class="label label-primary">BeamBot SubNotify:' + SubscribeNotify +'</b>';
+         html += '<br><b><span class="label label-primary">TweetNotify:' + TweetNotify +'</b>';
+         html += '<br><b><span class="label label-primary">Transcoding:' + te +'</b>';
+         html += '<br><b><span class="label label-warning">------------</b>'
          html += "<br>";
          if(Online){
              html += '<br><b><font color="White"><a href="https://beam.pro/' + Username + '">Online</a></font></b>';
